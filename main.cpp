7baaa1ba3e9b6778c9eb3698b20832551a747d06
Rainbow <sunsijie1234@foxmail.com>
2014-07-23 19:38:02 +0800

    restore deleted main.cpp wrongly

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: main.cpp
+# Author: Rainbow
+# mail: 443152178@qq.com
+    > Created Time: Wed 23 Jul 2014 05:57:57 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
